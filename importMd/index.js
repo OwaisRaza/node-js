b96d57177d427399894c8ea809e039a80cf53ce0
Owais Raza khanzada <owaisraza1989@gmail.com>
2021-08-27 19:13:24 +0500

    added multiple method and exports in other file

--- importMd/index.js
+++ importMd/index.js
@@ -1,2 +1,5 @@
-const add = require("./opra");
+const { add, sub, multi } = require("./opra");
+
 console.log(add(5, 20));
+console.log(sub(10, 5));
+console.log(multi(5, 20));
--- importMd/opra.js
+++ importMd/opra.js
@@ -2,4 +2,16 @@ const add = (a, b) => {
   return a + b;
 };
 
-module.exports = add;
+const sub = (a, b) => {
+  return a - b;
+};
+
+const multi = (a, b) => {
+  return a * b;
+};
+
+// module.exports.add = add;
+// module.exports.sub = sub;
+// module.exports.multi = multi;
+
+module.exports = { add, sub, multi };
