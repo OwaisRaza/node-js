db745153c31ad76b449b66a31452b40c83a694ef
Owais Raza khanzada <owaisraza1989@gmail.com>
2021-08-30 16:47:01 +0500

    handle server routing

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,7 +1,14 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hi ovais raza, how are you ?");
+  if (req.url === "/") {
+    res.end("Hi ovais raza, how are you ?");
+  } else if (req.url === "/about") {
+    res.end("welcome About us page");
+  } else {
+    res.writeHead("404", { content_type: "text/html" });
+    res.end("<h1>404 Not found!</h1>");
+  }
 });
 
 server.listen("8000", "localhost", () => {
