6e77bfd70afefc123cc55c90f45598ab733233ba
Owais Raza khanzada <owaisraza1989@gmail.com>
2021-08-31 16:32:19 +0500

    added event basic example

--- nodeEvent/index.js
+++ nodeEvent/index.js
@@ -6,4 +6,9 @@ event.on("checkStatus", (code, status) => {
   console.log(`status code is ${code} and the page is ${status}`);
 });
 
+event.on("fullName", () => {
+  console.log("my name is ovais raza");
+});
+
 event.emit("checkStatus", 200, "ok");
+event.emit("fullName");
