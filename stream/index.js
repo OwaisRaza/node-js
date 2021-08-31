a44139bb67f12f8e7fad79ff1dab626cbb750455
Owais Raza khanzada <owaisraza1989@gmail.com>
2021-08-31 17:46:56 +0500

    node stream practice

---
+++ stream/index.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const http = require("http");
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  const readStream = fs.createReadStream("input.txt");
+
+  readStream.on("data", (chunk) => {
+    res.write(chunk);
+  });
+
+  readStream.on("end", () => {
+    res.end();
+  });
+
+  readStream.on("error", (err) => {
+    console.log(err);
+    res.end("file not found!");
+  });
+});
+
+server.listen("8000", "localhost", () => {
+  console.log("listing....");
+});
